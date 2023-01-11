415aa1e44e66acc7d168d55e4648c507408b66ca
shoyaokayama <sho.edu.human@gmail.com>
2023-01-11 19:00:26 +0900

    :sparkles: add KamonInterceptor

--- src/main/scala/HelloWorldServer.scala
+++ src/main/scala/HelloWorldServer.scala
@@ -1,7 +1,22 @@
-import io.grpc.{Server, ServerBuilder}
+import io.grpc.{
+  ForwardingServerCall,
+  Metadata,
+  Server,
+  ServerBuilder,
+  ServerCall,
+  ServerCallHandler,
+  ServerInterceptor,
+  Status
+}
 import kamon.Kamon
 import scala.concurrent.{ExecutionContext, Future}
-import sample.helloworld.{GreeterGrpc, HelloReply, HelloRequest}
+import sample.helloworld.{
+  GoodbyeReply,
+  GoodbyeRequest,
+  GreeterGrpc,
+  HelloReply,
+  HelloRequest
+}
 
 object HelloWorldServer {
   def main(args: Array[String]): Unit = {
@@ -17,17 +32,51 @@ class HelloWorldServer(executionContext: ExecutionContext) { self =>
     ServerBuilder
       .forPort(HelloWorldServer.port)
       .addService(GreeterGrpc.bindService(new GreeterImpl, executionContext))
+      .intercept(new KamonInterceptor())
       .build()
       .start
   }
 
   private class GreeterImpl extends GreeterGrpc.Greeter {
     override def sayHello(req: HelloRequest): Future[HelloReply] = {
-      val span = Kamon.spanBuilder("sayHello").start()
       val reply = HelloReply(message = "Hello " + req.name)
       println(reply)
-      span.finish()
       Future.successful(reply)
+    }
+
+    override def sayGoodbye(req: GoodbyeRequest): Future[GoodbyeReply] = {
+      val reply = GoodbyeReply(message = "Goodbye " + req.name)
+      println(reply)
+      Future.successful(reply)
+    }
+  }
+
+  private class KamonInterceptor extends ServerInterceptor {
+
+    override def interceptCall[ReqT, RespT](
+        call: ServerCall[ReqT, RespT],
+        headers: Metadata,
+        next: ServerCallHandler[ReqT, RespT]
+    ): ServerCall.Listener[ReqT] = {
+      val span =
+        Kamon.spanBuilder(call.getMethodDescriptor.getBareMethodName).start()
+
+      def wrapperCall: ServerCall[ReqT, RespT] =
+        new ForwardingServerCall.SimpleForwardingServerCall[ReqT, RespT](
+          call
+        ) {
+          override def request(numMessages: Int): Unit = {
+            println("pre request")
+            super.request(numMessages)
+          }
+
+          override def close(status: Status, trailers: Metadata): Unit = {
+            println("close request")
+            span.finish()
+            super.close(status, trailers)
+          }
+        }
+      next.startCall(wrapperCall, headers)
     }
   }
 
